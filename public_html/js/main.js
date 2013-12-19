bbda64bc67135bed849074e3c056dd5f8f8beabd
Tushar Roy <xxcrack@gmail.com>
2013-12-19 16:40:27 +0600

    so many changes

---
+++ public_html/js/main.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function incorrectAnswer()
+{
+    var $messageDiv = $('#message'); // get the reference of the div
+    $messageDiv.show().html('X'); // show and set the message
+    setTimeout(function() {
+        $messageDiv.hide().html('');
+    }, 1800); // 3 seconds later, hide
+    // and clear the message
+}
